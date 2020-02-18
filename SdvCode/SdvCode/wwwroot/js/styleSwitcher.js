5e6bef4a589c7306e773ab55b1afb887a6a606f4
Simeon Valev <simeonvalev00@gmail.com>
2020-02-18 13:15:55 +0200

    Make login and register layout

---
+++ SdvCode/SdvCode/wwwroot/js/styleSwitcher.js
@@ -0,0 +1,51 @@
+document.getElementById("preset1").addEventListener("click", function (e) {
+    e.preventDefault();
+    document.getElementById("style-switch").href = "css/presets/preset1.css";
+});
+document.getElementById("preset2").addEventListener("click", function (e) {
+    e.preventDefault();
+    document.getElementById("style-switch").href = "css/presets/preset2.css";
+});
+document.getElementById("preset3").addEventListener("click", function (e) {
+    e.preventDefault();
+    document.getElementById("style-switch").href = "css/presets/preset3.css";
+});
+document.getElementById("preset4").addEventListener("click", function (e) {
+    e.preventDefault();
+    document.getElementById("style-switch").href = "css/presets/preset4.css";
+});
+document.getElementById("preset5").addEventListener("click", function (e) {
+    e.preventDefault();
+    document.getElementById("style-switch").href = "css/presets/preset5.css";
+});
+document.getElementById("preset6").addEventListener("click", function (e) {
+    e.preventDefault();
+    document.getElementById("style-switch").href = "css/presets/preset6.css";
+});
+//jQuery(document).ready(function ($) {
+//	// Color Changer
+//	$("#preset1").click(function () {
+//		$("#style-switch").attr("href", "~/css/presets/preset1.css");
+//		return false;
+//	});
+//	$("#preset2").click(function () {
+//		$("#style-switch").attr("href", "~/css/presets/preset2.css");
+//		return false;
+//	});
+//	$("#preset3").click(function () {
+//		$("#style-switch").attr("href", "~/css/presets/preset3.css");
+//		return false;
+//	});
+//	$("#preset4").click(function () {
+//		$("#style-switch").attr("href", "~/css/presets/preset4.css");
+//		return false;
+//	});
+//	$("#preset5").click(function () {
+//		$("#style-switch").attr("href", "~/css/presets/preset5.css");
+//		return false;
+//	});
+//	$("#preset6").click(function () {
+//		$("#style-switch").attr("href", "~/css/presets/preset6.css");
+//		return false;
+//	});
+//});
