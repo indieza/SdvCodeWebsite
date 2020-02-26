5fff5b34c3e22a843a39ccc1407fe99121479c9d
Simeon Valev <simeonvalev00@gmail.com>
2020-02-26 18:31:01 +0200

    Make logout in nav bar

--- SdvCode/SdvCode/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ SdvCode/SdvCode/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -23,8 +23,11 @@ namespace SdvCode.Areas.Identity.Pages.Account
             _logger = logger;
         }
 
-        public void OnGet()
+        public async Task<IActionResult> OnGet()
         {
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation("User logged out.");
+            return this.Redirect("/");
         }
 
         public async Task<IActionResult> OnPost(string returnUrl = null)
