ce10498e43407cd7836ff9cba0177b2a0c313fe3
Simeon Valev <simeonvalev00@gmail.com>
2020-02-27 17:55:08 +0200

    Word on 404 error page

---
+++ SdvCode/SdvCode/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SdvCode.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("/Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            return View("NotFound");
+        }
+    }
+}
--- SdvCode/SdvCode/Startup.cs
+++ SdvCode/SdvCode/Startup.cs
@@ -53,6 +53,7 @@ namespace SdvCode
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseStatusCodePagesWithRedirects("/Error/{0}");
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
