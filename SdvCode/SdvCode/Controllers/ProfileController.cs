9288378f4c2888073750ce4176baf4cf80bdb5d8
Simeon Valev <simeonvalev00@gmail.com>
2020-03-01 18:47:14 +0200

    Work on profile page

--- SdvCode/SdvCode/Controllers/ProfileController.cs
+++ SdvCode/SdvCode/Controllers/ProfileController.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using SdvCode.Data;
+using SdvCode.Models;
 
 namespace SdvCode.Controllers
 {
     public class ProfileController : Controller
     {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public ProfileController(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
         [Authorize]
         public IActionResult Index()
         {
-            return View();
+            var id = this.userManager.GetUserId(HttpContext.User);
+            var user = this.userManager.FindByIdAsync(id).Result;
+            return View(user);
         }
     }
 }
