b011b08538a4b4f6638b5bc055638c9b3f4c8a89
Simeon Valev <simeonvalev00@gmail.com>
2020-03-01 17:19:33 +0200

    Make profile page design

---
+++ SdvCode/SdvCode/Controllers/ProfileController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SdvCode.Controllers
+{
+    public class ProfileController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
