a5ba680debddde71bd7a5873afb187f3b70bbab2
Simeon Valev <simeonvalev00@gmail.com>
2020-03-05 01:48:58 +0200

    Edit upload buttons for user images

--- SdvCode/SdvCode/wwwroot/js/profileScripts.js
+++ SdvCode/SdvCode/wwwroot/js/profileScripts.js
@@ -5,3 +5,27 @@
         document.getElementById("profilePhoneNumber").disabled = false;
     }
 }
+
+$('#chooseProfileFile').bind('change', function () {
+    var filename = $("#chooseProfileFile").val();
+    if (/^\s*$/.test(filename)) {
+        $("#chooseProfileImage").removeClass('active');
+        $("#noFile").text("No file chosen...");
+    }
+    else {
+        $("#chooseProfileImage").addClass('active');
+        $("#noProfileFile").text(filename.replace("C:\\fakepath\\", ""));
+    }
+});
+
+$('#chooseCoverFile').bind('change', function () {
+    var filename = $("#chooseCoverFile").val();
+    if (/^\s*$/.test(filename)) {
+        $("#chooseCoverImage").removeClass('active');
+        $("#noCoverFile").text("No file chosen...");
+    }
+    else {
+        $("#chooseCoverImage").addClass('active');
+        $("#noCoverFile").text(filename.replace("C:\\fakepath\\", ""));
+    }
+});
