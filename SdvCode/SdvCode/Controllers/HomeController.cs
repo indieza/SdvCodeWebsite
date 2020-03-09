c8a1f9d0110f4544317b4f767aace47031230b2f
Simeon Valev <simeonvalev00@gmail.com>
2020-03-09 04:20:13 +0200

    Make if statement fro admin panel

--- SdvCode/SdvCode/Controllers/HomeController.cs
+++ SdvCode/SdvCode/Controllers/HomeController.cs
@@ -23,11 +23,6 @@ namespace SdvCode.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
