b872a79552bcf96218c0d268a218fbc6e18bb077
Simeon Valev <simeonvalev00@gmail.com>
2020-03-16 22:39:33 +0200

    Inject TinyMCE - left to find free version

---
+++ SdvCode/SdvCode/wwwroot/js/tinyMce.js
@@ -0,0 +1,8 @@
+﻿tinymce.init({
+    selector: 'textarea',
+    plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
+    toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table',
+    toolbar_mode: 'floating',
+    tinycomments_mode: 'embedded',
+    tinycomments_author: 'Author name',
+});
