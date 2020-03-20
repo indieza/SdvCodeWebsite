e83d0ec5a22c07080d927d0c2e466cd5992fab94
Simeon Valev <simeonvalev00@gmail.com>
2020-03-20 20:15:11 +0200

    Update database model

--- SdvCode/SdvCode/Models/Blog/PostLike.cs
+++ SdvCode/SdvCode/Models/Blog/PostLike.cs
@@ -4,14 +4,22 @@
 namespace SdvCode.Models.Blog
 {
     using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using SdvCode.Models.User;
 
     public class PostLike
     {
         [Required]
+        [ForeignKey(nameof(ApplicationUser))]
         public string UserId { get; set; }
 
+        public ApplicationUser ApplicationUser { get; set; }
+
         [Required]
+        [ForeignKey(nameof(Post))]
         public string PostId { get; set; }
+
+        public Post Post { get; set; }
 
         public bool IsLiked { get; set; }
     }
