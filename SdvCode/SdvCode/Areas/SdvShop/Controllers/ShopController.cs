0787401de1abb5a6dd32650e1580813eb00c3daa
Simeon Valev <simeonvalev00@gmail.com>
2020-03-23 21:23:08 +0200

    Connect shop to main menu, add delete confirmation for post deleting

--- SdvCode/SdvCode/Areas/SdvShop/Controllers/ShopController.cs
+++ SdvCode/SdvCode/Areas/SdvShop/Controllers/ShopController.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) SDV Code Project. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace SdvCode.Areas.SdvCodeShop.Controllers
+namespace SdvCode.Areas.SdvShop.Controllers
 {
     using System;
     using System.Collections.Generic;
@@ -9,7 +9,7 @@ namespace SdvCode.Areas.SdvCodeShop.Controllers
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
 
-    [Area("SdvCodeShop")]
+    [Area("SdvShop")]
     public class ShopController : Controller
     {
         public IActionResult Index()
