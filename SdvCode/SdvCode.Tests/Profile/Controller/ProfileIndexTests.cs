267a913fcf510ade0da1dbfd9615b1cbe77e8d7c
Simeon Valev <simeonvalev00@gmail.com>
2020-03-30 23:16:58 +0300

    Make some trashes on unit testing

---
+++ SdvCode/SdvCode.Tests/Profile/Controller/ProfileIndexTests.cs
@@ -0,0 +1,27 @@
+﻿namespace SdvCode.Tests.Profile.Controller
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Moq;
+    using SdvCode.Controllers;
+    using SdvCode.Models.Enums;
+    using SdvCode.Models.User;
+    using SdvCode.Services.Profile;
+    using SdvCode.ViewModels.Users.ViewModels;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class ProfileIndexTests
+    {
+        [Fact]
+        public async Task IndexShouldReturnCorrectViewModel()
+        {
+            var mockService = new Mock<IProfileService>();
+            mockService.Setup(x => x.DeleteActivityById(new DefaultHttpContext(), 2)).ReturnsAsync("Follow");
+        }
+    }
+}
