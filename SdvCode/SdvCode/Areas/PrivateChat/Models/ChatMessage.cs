42bf80f0fda80baced6822ed715080776559053f
Simeon Valev <simeonvalev00@gmail.com>
2020-03-30 13:56:24 +0300

    Start work on chat entities

---
+++ SdvCode/SdvCode/Areas/PrivateChat/Models/ChatMessage.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) SDV Code Project. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace SdvCode.Areas.PrivateChat.Models
+{
+    using SdvCode.Models.User;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class ChatMessage
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(550)]
+        public string Content { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Group))]
+        public int GroupId { get; set; }
+
+        public Group Group { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(ApplicationUser))]
+        public string ApplicationUserId { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
---
+++ SdvCode/SdvCode/Areas/PrivateChat/Models/Group.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) SDV Code Project. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace SdvCode.Areas.PrivateChat.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Reflection;
+    using System.Threading.Tasks;
+
+    public class Group
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        public ICollection<UserGroup> UsersGroups { get; set; } = new HashSet<UserGroup>();
+
+        public ICollection<ChatMessage> ChatMessages { get; set; } = new HashSet<ChatMessage>();
+    }
+}
