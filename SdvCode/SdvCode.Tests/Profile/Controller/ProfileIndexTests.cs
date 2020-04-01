36e1c4048997a0a17a1c4ac99af2786fed8ebc89
Simeon Valev <simeonvalev00@gmail.com>
2020-04-01 12:26:28 +0300

    Extend profile controller test

--- SdvCode/SdvCode.Tests/Profile/Controller/ProfileIndexTests.cs
+++ SdvCode/SdvCode.Tests/Profile/Controller/ProfileIndexTests.cs
@@ -3,11 +3,14 @@
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Options;
     using Moq;
     using SdvCode.Controllers;
     using SdvCode.Models.Enums;
     using SdvCode.Models.User;
     using SdvCode.Services.Profile;
+    using SdvCode.ViewModels.Profile;
     using SdvCode.ViewModels.Users.ViewModels;
     using System;
     using System.Collections.Generic;
@@ -21,7 +24,26 @@
         public async Task IndexShouldReturnCorrectViewModel()
         {
             var mockService = new Mock<IProfileService>();
-            mockService.Setup(x => x.DeleteActivityById(new DefaultHttpContext(), 2)).ReturnsAsync("Follow");
+            var mockUserManager = new Mock<UserManager<ApplicationUser>>(
+                    new Mock<IUserStore<ApplicationUser>>().Object,
+                    new Mock<IOptions<IdentityOptions>>().Object,
+                    new Mock<IPasswordHasher<ApplicationUser>>().Object,
+                    new IUserValidator<ApplicationUser>[0],
+                    new IPasswordValidator<ApplicationUser>[0],
+                    new Mock<ILookupNormalizer>().Object,
+                    new Mock<IdentityErrorDescriber>().Object,
+                    new Mock<IServiceProvider>().Object,
+                    new Mock<ILogger<UserManager<ApplicationUser>>>().Object);
+
+            var controller = new ProfileController(mockUserManager.Object, mockService.Object);
+            var result = await controller.Index("indieza", ProfileTab.Activities, 0);
+            Assert.IsType<ViewResult>(result);
+
+            var viewResult = result as ViewResult;
+            Assert.IsType<ProfileViewModel>(viewResult.Model);
+
+            var viewModel = viewResult.Model as ProfileViewModel;
+            Assert.Equal(ProfileTab.Activities, viewModel.ActiveTab);
         }
     }
 }
