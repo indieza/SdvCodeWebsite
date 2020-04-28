4a7fdd039cee84e69f77bb77c4f403bfdf6855f3
Simeon Valev <simeonvalev00@gmail.com>
2020-04-28 11:05:55 +0300

    Apply db migration for order improvement

---
+++ SdvCode/SdvCode/Data/Migrations/20200428080425_ImproveOrderDbModel.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) SDV Code Project. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace SdvCode.Data.Migrations
+{
+    using System;
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class ImproveOrderDbModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CanceledOn",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Orders",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FinishedOn",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderStatus",
+                table: "Orders",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CanceledOn",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "FinishedOn",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "OrderStatus",
+                table: "Orders");
+        }
+    }
+}
