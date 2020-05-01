7652aaf196f37b5f40ee4fbcba16cf4bb960d5b7
Simeon Valev <simeonvalev00@gmail.com>
2020-05-01 13:08:55 +0300

    Implement website theme change and save in local storage

--- SdvCode/SdvCode/wwwroot/js/styleSwitcher.js
+++ SdvCode/SdvCode/wwwroot/js/styleSwitcher.js
@@ -1,24 +1,38 @@
+function setSiteTheme() {
+    let theme = localStorage.getItem("SdvCodeTheme");
+    if (!theme) {
+        localStorage.setItem("SdvCodeTheme", "/css/presets/preset1.css");
+    }
+    document.getElementById("style-switch").href = localStorage.getItem("SdvCodeTheme");
+}
+
 document.getElementById("preset1").addEventListener("click", function (e) {
     e.preventDefault();
+    localStorage.setItem("SdvCodeTheme", "/css/presets/preset1.css");
     document.getElementById("style-switch").href = "/css/presets/preset1.css";
 });
 document.getElementById("preset2").addEventListener("click", function (e) {
     e.preventDefault();
+    localStorage.setItem("SdvCodeTheme", "/css/presets/preset2.css");
     document.getElementById("style-switch").href = "/css/presets/preset2.css";
 });
 document.getElementById("preset3").addEventListener("click", function (e) {
     e.preventDefault();
+    localStorage.setItem("SdvCodeTheme", "/css/presets/preset3.css");
     document.getElementById("style-switch").href = "/css/presets/preset3.css";
 });
 document.getElementById("preset4").addEventListener("click", function (e) {
     e.preventDefault();
+    localStorage.setItem("SdvCodeTheme", "/css/presets/preset4.css");
     document.getElementById("style-switch").href = "/css/presets/preset4.css";
 });
 document.getElementById("preset5").addEventListener("click", function (e) {
     e.preventDefault();
+    localStorage.setItem("SdvCodeTheme", "/css/presets/preset5.css");
     document.getElementById("style-switch").href = "/css/presets/preset5.css";
 });
 document.getElementById("preset6").addEventListener("click", function (e) {
     e.preventDefault();
+    localStorage.setItem("SdvCodeTheme", "/css/presets/preset6.css");
     document.getElementById("style-switch").href = "/css/presets/preset6.css";
 });
