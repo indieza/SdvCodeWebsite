c48612bf0a6af12ecab1791519f09104de13f0e6
Simeon Valev <simeonvalev00@gmail.com>
2020-06-11 17:45:35 +0300

    Finish user status hub logic

--- SdvCode/SdvCode/Hubs/UserStatusHub.cs
+++ SdvCode/SdvCode/Hubs/UserStatusHub.cs
@@ -8,19 +8,50 @@ namespace SdvCode.Hubs
     using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.SignalR;
+    using Microsoft.EntityFrameworkCore;
+    using SdvCode.Data;
 
     public class UserStatusHub : Hub
     {
-        public override Task OnConnectedAsync()
+        private static readonly List<string> OnlineUsers = new List<string>();
+        private readonly ApplicationDbContext db;
+
+        public UserStatusHub(ApplicationDbContext db)
         {
-            var username = this.Context.User.Identity.Name;
-            return base.OnConnectedAsync();
+            this.db = db;
         }
 
-        public override Task OnDisconnectedAsync(Exception exception)
+        public async Task IsUserOnline(string username)
+        {
+            var user = await this.db.Users.FirstOrDefaultAsync(x => x.UserName.ToLower() == username.ToLower());
+            if (OnlineUsers.Contains(user.UserName))
+            {
+                await this.Clients.All.SendAsync("UserIsOnline", user.UserName);
+            }
+            else
+            {
+                await this.Clients.All.SendAsync("UserIsOffline", user.UserName);
+            }
+        }
+
+        public override async Task OnConnectedAsync()
         {
             var username = this.Context.User.Identity.Name;
-            return base.OnDisconnectedAsync(exception);
+            if (username != null)
+            {
+                OnlineUsers.Add(username);
+                await this.Clients.All.SendAsync("UserIsOnline", username);
+            }
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            var username = this.Context.User.Identity.Name;
+            if (username != null)
+            {
+                OnlineUsers.Remove(username);
+                await this.Clients.All.SendAsync("UserIsOffline", username);
+            }
         }
     }
 }
--- SdvCode/SdvCode/wwwroot/userStatus/js/userStatus.js
+++ SdvCode/SdvCode/wwwroot/userStatus/js/userStatus.js
@@ -3,7 +3,24 @@
 let userStatusConnection = new signalR.HubConnectionBuilder().withUrl("/userStatusHub").build();
 
 userStatusConnection.start().then(function () {
-    console.log("connected");
+    let h5 = document.getElementById("currentUsername");
+    if (h5) {
+        let username = h5.innerText.substr(1, h5.innerText.length);
+        userStatusConnection.invoke("IsUserOnline", username)
+            .catch(function (err) {
+                return console.error(err.toString());
+            });;
+    }
 }).catch(function (err) {
     return console.error(err.toString());
+});
+
+userStatusConnection.on("UserIsOnline", function (username) {
+    document.getElementById(`${username}userSatus`).innerText = "Online";
+    document.getElementById("userStatusDot").style.backgroundColor = "green";
+});
+
+userStatusConnection.on("UserIsOffline", function (username) {
+    document.getElementById(`${username}userSatus`).innerText = "Offline";
+    document.getElementById("userStatusDot").style.backgroundColor = "orange";
 });
