d048473eaf0fcd8a708805156034d9e005872671
indieza <simeonvalev00@gmail.com>
2020-11-10 13:09:54 +0200

    Finish the logic for emojis visualization

--- SdvCode/SdvCode/wwwroot/js/emojiScript.js
+++ SdvCode/SdvCode/wwwroot/js/emojiScript.js
@@ -1,4 +1,46 @@
-﻿function popUpEmoji() {
-    var popup = document.getElementById("popupEmoji");
-    popup.classList.toggle("show");
+﻿$(document).ready(function () {
+    $('#emojiButton').click(function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $('#popupEmoji').toggleClass("show");
+    });
+
+    $('#popupEmoji').click(function (e) {
+        e.stopPropagation();
+    });
+
+    $('body').click(function () {
+        $('#popupEmoji').removeClass("show");
+    });
+});
+
+function popUpEmoji() {
+    let popup = document.getElementById("popupEmoji");
+    popup.classList.add("show");
+}
+
+function changeEmojisTabs(emojiType) {
+    let tabs = document.querySelectorAll(".emoji-type-tab");
+
+    for (let tab of tabs) {
+        if (tab.children[0].id == `${emojiType}-Tab`) {
+            tab.children[0].style.backgroundColor = "#727272";
+        } else {
+            tab.children[0].style.backgroundColor = "";
+        }
+    }
+
+    let tabsSections = document.querySelectorAll(".tab-section");
+
+    for (let tabSection of tabsSections) {
+        if (tabSection.id == `${emojiType}-Tab-Section`) {
+            tabSection.style.display = "block";
+        } else {
+            tabSection.style.display = "none";
+        }
+    }
+}
+
+function addEmoji(emojiSpan) {
+    document.getElementById("messageInput").value += emojiSpan.innerText;
 }
