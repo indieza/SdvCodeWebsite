7c9314255b4b7f144bad1fac7caf20ea9e3a6cc1
indieza <simeonvalev00@gmail.com>
2020-11-20 18:31:19 +0200

    Implement design of the select images button in private chat

---
+++ SdvCode/SdvCode/wwwroot/js/chatImagesScript.js
@@ -0,0 +1,17 @@
+﻿$("#imageButton i").click(function () {
+    $("#uploadImage").trigger('click');
+});
+
+$('#uploadImage').on('change', function () {
+    let filesCount = document.getElementById("uploadImage").files.length;
+    let badge = document.querySelector(".select-image-badge");
+    badge.innerText = filesCount;
+
+    if (filesCount > 0) {
+        badge.style.boxShadow = "0 0 0 0 rgba(51, 217, 178, 1)";
+        badge.style.animation = "pulse-green 2s infinite";
+    } else {
+        badge.style.boxShadow = "";
+        badge.style.animation = "";
+    }
+})
