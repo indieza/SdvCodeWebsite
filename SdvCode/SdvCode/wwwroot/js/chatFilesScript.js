7e11ee3112043656f820f65bbfb67992d37e7211
indieza <simeonvalev00@gmail.com>
2020-11-28 22:26:09 +0200

    Work on the logic for uploading none-image files

---
+++ SdvCode/SdvCode/wwwroot/js/chatFilesScript.js
@@ -0,0 +1,17 @@
+﻿$("#fileButton i").click(function () {
+    $("#uploadFile").trigger('click');
+});
+
+$('#uploadFile').on('change', function () {
+    let filesCount = document.getElementById("uploadFile").files.length;
+    let badge = document.querySelector(".select-file-badge");
+    badge.innerText = filesCount;
+
+    if (filesCount > 0) {
+        badge.style.boxShadow = "0 0 0 0 rgba(2, 191, 255, 1)";
+        badge.style.animation = "pulse-green 2s infinite";
+    } else {
+        badge.style.boxShadow = "";
+        badge.style.animation = "";
+    }
+})
