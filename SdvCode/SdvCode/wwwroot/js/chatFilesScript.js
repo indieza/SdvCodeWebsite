37e5c60f16ef3111a04696a0a43724cffe56b64e
indieza <simeonvalev00@gmail.com>
2020-11-29 15:15:49 +0200

    Finish the duplication of files logic

--- SdvCode/SdvCode/wwwroot/js/chatFilesScript.js
+++ SdvCode/SdvCode/wwwroot/js/chatFilesScript.js
@@ -5,11 +5,52 @@
 $('#uploadFile').on('change', function () {
     const fileExtensions = ["TXT", "TEXT", "DOCX", "DOC", "PDF", "PPT", "XLS", "XLSX"];
     let files = document.getElementById("uploadFile").files;
-    const dt = new DataTransfer();
+    const dtFiles = new DataTransfer();
+    const dtImages = new DataTransfer();
 
-    for (var file of files) {
+    for (let file of files) {
         let fileExtension = file.name.split('.').pop();
         if (fileExtensions.includes(fileExtension.toUpperCase())) {
+            dtFiles.items.add(file);
+        } else {
+            dtImages.items.add(file);
+        }
+    }
+
+    document.getElementById("uploadFile").files = dtFiles.files;
+    let filesCount = document.getElementById("uploadFile").files.length;
+    let badge = document.querySelector(".select-file-badge");
+    badge.innerText = filesCount;
+
+    if (filesCount > 0) {
+        badge.style.boxShadow = "0 0 0 0 rgba(2, 191, 255, 1)";
+        badge.style.animation = "pulse-green 2s infinite";
+    } else {
+        badge.style.boxShadow = "";
+        badge.style.animation = "";
+    }
+
+    if (dtImages.items.length > 0) {
+        transferImages(dtImages);
+    }
+});
+
+function transferFiles(dtFiles) {
+    const fileExtensions = ["TXT", "TEXT", "DOCX", "DOC", "PDF", "PPT", "XLS", "XLSX"];
+    let files = document.getElementById("uploadFile").files;
+    const dt = new DataTransfer();
+
+    for (let file of files) {
+        let fileExtension = file.name.split('.').pop();
+        if (fileExtensions.includes(fileExtension.toUpperCase())) {
+            dt.items.add(file);
+        }
+    }
+
+    for (let file of dtFiles.files) {
+        let fileExtension = file.name.split('.').pop();
+        let isFileExist = [...files].some(x => x.name == file.name);
+        if (fileExtensions.includes(fileExtension.toUpperCase()) && !isFileExist) {
             dt.items.add(file);
         }
     }
@@ -26,4 +67,4 @@ $('#uploadFile').on('change', function () {
         badge.style.boxShadow = "";
         badge.style.animation = "";
     }
-})
+}
--- SdvCode/SdvCode/wwwroot/js/chatImagesScript.js
+++ SdvCode/SdvCode/wwwroot/js/chatImagesScript.js
@@ -5,16 +5,19 @@
 $('#uploadImage').on('change', function () {
     const imageExtensions = ["JPG", "PNG", "JPEG"];
     let files = document.getElementById("uploadImage").files;
-    const dt = new DataTransfer();
+    const dtImages = new DataTransfer();
+    const dtFiles = new DataTransfer();
 
-    for (var file of files) {
+    for (let file of files) {
         let fileExtension = file.name.split('.').pop();
         if (imageExtensions.includes(fileExtension.toUpperCase())) {
-            dt.items.add(file);
+            dtImages.items.add(file);
+        } else {
+            dtFiles.items.add(file);
         }
     }
 
-    document.getElementById("uploadImage").files = dt.files;
+    document.getElementById("uploadImage").files = dtImages.files;
     let filesCount = document.getElementById("uploadImage").files.length;
     let badge = document.querySelector(".select-image-badge");
     badge.innerText = filesCount;
@@ -26,4 +29,42 @@ $('#uploadImage').on('change', function () {
         badge.style.boxShadow = "";
         badge.style.animation = "";
     }
-})
+
+    if (dtFiles.items.length > 0) {
+        transferFiles(dtFiles);
+    }
+});
+
+function transferImages(dtImages) {
+    const imageExtensions = ["JPG", "PNG", "JPEG"];
+    let images = document.getElementById("uploadImage").files;
+    const dt = new DataTransfer();
+
+    for (let image of images) {
+        let fileExtension = image.name.split('.').pop();
+        if (imageExtensions.includes(fileExtension.toUpperCase())) {
+            dt.items.add(image);
+        }
+    }
+
+    for (let image of dtImages.files) {
+        let fileExtension = image.name.split('.').pop();
+        let isFileExist = [...images].some(x => x.name == image.name);
+        if (imageExtensions.includes(fileExtension.toUpperCase()) && !isFileExist) {
+            dt.items.add(image);
+        }
+    }
+
+    document.getElementById("uploadImage").files = dt.files;
+    let imagesCount = document.getElementById("uploadImage").files.length;
+    let badge = document.querySelector(".select-image-badge");
+    badge.innerText = imagesCount;
+
+    if (imagesCount > 0) {
+        badge.style.boxShadow = "0 0 0 0 rgba(2, 191, 255, 1)";
+        badge.style.animation = "pulse-green 2s infinite";
+    } else {
+        badge.style.boxShadow = "";
+        badge.style.animation = "";
+    }
+}
