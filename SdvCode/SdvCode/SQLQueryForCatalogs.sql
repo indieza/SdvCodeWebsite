72917f36ba9a697ec37f00646534fbbba3a4a051
indieza <simeonvalev00@gmail.com>
2020-11-30 23:19:07 +0200

    Add Query for creating a SQL Catalogues for Full-Text INDEX Search

---
+++ SdvCode/SdvCode/SQLQueryForCatalogs.sql
@@ -0,0 +1,17 @@
+/*Catalog for Users*/
+CREATE FULLTEXT CATALOG UsersCatalog AS DEFAULT;
+CREATE FULLTEXT INDEX
+                   ON [AspNetUsers]([UserName], [FirstName], [LastName])
+            KEY INDEX PK_AspNetUsers;
+
+/*Catalog for Posts*/
+CREATE FULLTEXT CATALOG PostsCatalog AS DEFAULT;
+CREATE FULLTEXT INDEX
+                   ON [Posts]([Content], [ShortContent])
+            KEY INDEX PK_Posts;
+
+/*Catalog for Products*/
+CREATE FULLTEXT CATALOG ProductsCatalog AS DEFAULT;
+CREATE FULLTEXT INDEX
+                   ON [Products]([Description], [Name], [SpecificationsDescription])
+	        KEY INDEX PK_Products;
