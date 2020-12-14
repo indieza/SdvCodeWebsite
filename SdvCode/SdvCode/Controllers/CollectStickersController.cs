14c3acde8eacedb3fb7f23b76d1f0e3e811d5ca6
indieza <simeonvalev00@gmail.com>
2020-12-14 09:23:54 +0200

    Make controller and the view for collect stickers logic

---
+++ SdvCode/SdvCode/Controllers/CollectStickersController.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) SDV Code Project. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace SdvCode.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize]
+    public class CollectStickersController : Controller
+    {
+        public CollectStickersController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
