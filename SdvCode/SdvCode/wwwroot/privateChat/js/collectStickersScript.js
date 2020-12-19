76b867cbe2a2889c1eeb9ed8d584d2c238fe3552
indieza <simeonvalev00@gmail.com>
2020-12-19 16:57:53 +0200

    Finish the design of collect stickers page

---
+++ SdvCode/SdvCode/wwwroot/privateChat/js/collectStickersScript.js
@@ -0,0 +1,4 @@
+﻿$(".collapseIconSticker").on('click', function () {
+    $(this).children('.fa-chevron-down, .fa-chevron-up').toggleClass("fa-chevron-down fa-chevron-up");
+    $(this).toggleClass("activeCollapse")
+});
