8f4d802533efad832090f157f2ff14893793047e
Simeon Valev <simeonvalev00@gmail.com>
2022-05-21 12:11:25 +0300

    Work over data models

---
+++ SdvCode/SdvCode.Models/User/User.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="User.cs" company="SDV Code">
+// Copyright (c) SDV Code. All rights reserved.
+// </copyright>
+
+namespace SdvCode.Models.User
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Identity;
+
+    public class User : IdentityUser
+    {
+    }
+}
